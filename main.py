8beed99e9032b938f86dbb4142d29df16dc1d7fd
ARNABDHUA <arnabdhua74@gmail.com>
2026-04-09 09:00:15 +0530

    port update

--- main.py
+++ main.py
@@ -275,5 +275,9 @@ async def pdf_to_word(file: UploadFile = File(...)):
                              media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
                              headers={"Content-Disposition": "attachment; filename=pdf_to_word.docx"})
 
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
